5c6afac4d1bd83fc09a7e5d787c3f33ab3d53a65
crxxtian <124479510+crxxtian@users.noreply.github.com>
2023-07-11 19:37:35 -0500

    new

--- projekt.py
+++ projekt.py
@@ -1,83 +1,100 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QTextEdit, QPushButton, QVBoxLayout, QWidget, QFileDialog, QMessageBox
-from PyQt5.QtGui import QFont, QTextCharFormat, QTextCursor, QColor, QTextDocument
+import tkinter as tk
+from tkinter import filedialog, messagebox
 import openai
 import threading
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters import HtmlFormatter
 
 openai.api_key = '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'
 
-class SecurityScannerGUI(QMainWindow):
-    def __init__(self):
-        super().__init__()
 
-        self.setWindowTitle("Enterprise Code Security Scanner")
-        self.setGeometry(100, 100, 800, 600)
+class SecurityScannerGUI:
+    def __init__(self, root):
+        self.root = root
+        root.title("Enterprise Code Security Scanner")
+        root.geometry("800x600")
 
         self.init_ui()
 
         self.scan_results = ""
+        self.remediated_code = ""
 
     def init_ui(self):
-        main_widget = QWidget()
+        title_label = tk.Label(self.root, text="Enterprise Code Security Scanner", font=("Arial", 16, "bold"))
+        title_label.pack(pady=10)
 
-        vbox = QVBoxLayout()
+        instructions_label = tk.Label(self.root, text="Please enter the code you wish to scan or click 'Browse File' to select a file:")
+        instructions_label.pack()
 
-        title_label = QLabel("Enterprise Code Security Scanner")
-        title_font = QFont("Arial", 16, QFont.Bold)
-        title_label.setFont(title_font)
-        vbox.addWidget(title_label)
+        code_frame = tk.Frame(self.root)
+        code_frame.pack(pady=10)
 
-        instructions_label = QLabel("Please enter the code you wish to scan or click 'Browse File' to select a file:")
-        vbox.addWidget(instructions_label)
+        code_scrollbar = tk.Scrollbar(code_frame)
+        code_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
-        self.code_text = QTextEdit()
-        self.code_text.setLineWrapMode(QTextEdit.NoWrap)
-        vbox.addWidget(self.code_text)
+        self.code_text = tk.Text(code_frame, height=10, width=50, wrap=tk.NONE, yscrollcommand=code_scrollbar.set)
+        self.code_text.pack(side=tk.LEFT)
 
-        browse_button = QPushButton("Browse File")
-        browse_button.clicked.connect(self.browse_file)
-        vbox.addWidget(browse_button)
+        code_scrollbar.config(command=self.code_text.yview)
 
-        scan_button = QPushButton("Scan Code")
-        scan_button.clicked.connect(self.scan_code)
-        vbox.addWidget(scan_button)
+        browse_button = tk.Button(self.root, text="Browse File", command=self.browse_file)
+        browse_button.pack(pady=10)
 
-        save_button = QPushButton("Save Results")
-        save_button.clicked.connect(self.save_results)
-        vbox.addWidget(save_button)
+        scan_button = tk.Button(self.root, text="Scan Code", command=self.scan_code)
+        scan_button.pack()
 
-        load_button = QPushButton("Load Results")
-        load_button.clicked.connect(self.load_results)
-        vbox.addWidget(load_button)
+        result_frame = tk.Frame(self.root)
+        result_frame.pack(pady=10)
 
-        result_label = QLabel("Scan results and potential remediations will appear below:")
-        vbox.addWidget(result_label)
+        vulnerabilities_label = tk.Label(result_frame, text="Potential Vulnerabilities:")
+        vulnerabilities_label.pack(side=tk.LEFT)
 
-        self.result_text = QTextEdit()
-        self.result_text.setReadOnly(True)
-        vbox.addWidget(self.result_text)
+        remediation_label = tk.Label(result_frame, text="Remediated Code:")
+        remediation_label.pack(side=tk.LEFT, padx=10)
 
-        main_widget.setLayout(vbox)
-        self.setCentralWidget(main_widget)
+        vulnerabilities_scrollbar = tk.Scrollbar(self.root)
+        vulnerabilities_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+        self.vulnerabilities_text = tk.Text(self.root, height=20, width=40, wrap=tk.NONE, yscrollcommand=vulnerabilities_scrollbar.set)
+        self.vulnerabilities_text.pack(side=tk.LEFT, padx=10)
+        vulnerabilities_scrollbar.config(command=self.vulnerabilities_text.yview)
+
+        remediation_scrollbar = tk.Scrollbar(self.root)
+        remediation_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+        self.remediation_text = tk.Text(self.root, height=20, width=40, wrap=tk.NONE, yscrollcommand=remediation_scrollbar.set)
+        self.remediation_text.pack(side=tk.LEFT, padx=10)
+        remediation_scrollbar.config(command=self.remediation_text.yview)
+
+        save_button = tk.Button(self.root, text="Save Remediation", command=self.save_remediation)
+        save_button.pack(pady=10)
 
     def browse_file(self):
-        file_dialog = QFileDialog()
-        file_path, _ = file_dialog.getOpenFileName(self, "Select File to Scan", "", "All Files (*)")
+        file_path = filedialog.askopenfilename(title="Select File to Scan", filetypes=[("All Files", "*.*")])
         if file_path:
             with open(file_path, "r") as file:
                 code = file.read()
-                self.code_text.setPlainText(code)
+                self.code_text.delete("1.0", tk.END)
+                self.code_text.insert(tk.END, code)
 
     def scan_code(self):
-        code = self.code_text.toPlainText().strip()
+        code = self.code_text.get("1.0", tk.END).strip()
         if not code:
-            self.result_text.append("No code provided. Please enter code or select a file to scan.")
+            messagebox.showwarning("Scan Code", "No code provided. Please enter code or select a file to scan.")
             return
 
-        self.result_text.append("Scanning code...")
+        self.vulnerabilities_text.delete("1.0", tk.END)
+        self.remediation_text.delete("1.0", tk.END)
+
+        self.vulnerabilities_text.insert(tk.END, "Scanning code...\n")
+
         threading.Thread(target=self.perform_scan, args=(code,)).start()
 
     def perform_scan(self, code):
+        lexer = get_lexer_by_name("cpp")
+        formatter = HtmlFormatter(style="colorful")
+        highlighted_code = highlight(code, lexer, formatter)
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[
@@ -87,41 +104,37 @@ class SecurityScannerGUI(QMainWindow):
         )
         vulnerabilities = response.choices[0].message['content']
 
-        self.result_text.append("Potential vulnerabilities and remediations found within the code:")
-        self.result_text.append(vulnerabilities)
+        self.update_result_text(self.vulnerabilities_text, "Potential vulnerabilities and remediations found within the code:")
+        self.append_syntax_highlighted_text(self.vulnerabilities_text, highlighted_code)
+        self.update_result_text(self.vulnerabilities_text, vulnerabilities)
 
         self.scan_results = vulnerabilities
+        self.remediated_code = code
 
-    def save_results(self):
-        if not self.scan_results:
-            QMessageBox.information(self, "Save Results", "No scan results to save.")
+    def update_result_text(self, text_widget, text):
+        text_widget.insert(tk.END, text + "\n")
+        text_widget.see(tk.END)
+
+    def append_syntax_highlighted_text(self, text_widget, text):
+        text_widget.insert(tk.END, text)
+        text_widget.see(tk.END)
+
+    def save_remediation(self):
+        if not self.remediated_code:
+            messagebox.showwarning("Save Remediation", "No remediation code available.")
             return
 
-        file_dialog = QFileDialog()
-        file_path, _ = file_dialog.getSaveFileName(self, "Save Scan Results", "", "Text Files (*.txt)")
+        file_path = filedialog.asksaveasfilename(title="Save Remediation", filetypes=[("Text Files", "*.txt")])
         if file_path:
             try:
                 with open(file_path, "w") as file:
-                    file.write(self.scan_results)
-                QMessageBox.information(self, "Save Results", "Scan results saved successfully.")
+                    file.write(self.remediated_code)
+                messagebox.showinfo("Save Remediation", "Remediation code saved successfully.")
             except Exception as e:
-                QMessageBox.warning(self, "Save Results", f"Failed to save scan results.\nError: {str(e)}")
+                messagebox.showwarning("Save Remediation", f"Failed to save remediation code.\nError: {str(e)}")
 
-    def load_results(self):
-        file_dialog = QFileDialog()
-        file_path, _ = file_dialog.getOpenFileName(self, "Load Scan Results", "", "Text Files (*.txt)")
-        if file_path:
-            try:
-                with open(file_path, "r") as file:
-                    scan_results = file.read()
-                self.result_text.setPlainText(scan_results)
-                self.scan_results = scan_results
-                QMessageBox.information(self, "Load Results", "Scan results loaded successfully.")
-            except Exception as e:
-                QMessageBox.warning(self, "Load Results", f"Failed to load scan results.\nError: {str(e)}")
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    gui = SecurityScannerGUI()
-    gui.show()
-    sys.exit(app.exec_())
+    root = tk.Tk()
+    gui = SecurityScannerGUI(root)
+    root.mainloop()
